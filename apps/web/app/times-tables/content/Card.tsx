3502dac1a9244c752f1c116c80c88150a1af64ce
Safat khan <safat@kitegamesstudio.com>
2025-03-23 02:43:44 +0600

    Fix card size and position of times table cards.

--- apps/web/app/times-tables/content/Card.tsx
+++ apps/web/app/times-tables/content/Card.tsx
@@ -10,7 +10,7 @@ const Card: React.FC<CardProps> = ({ title, link, bgColor }) => {
   return (
     <Link href={link}>
       <div
-        className={`text-center p-3 rounded-xl shadow-md transition-transform transform hover:scale-105 hover:shadow-lg cursor-pointer border border-gray-200 ${bgColor}`}
+        className={`md:w-[200px] w-[250px] text-center  p-3 rounded-xl shadow-md transition-transform transform hover:scale-105 hover:shadow-lg cursor-pointer border border-gray-200 ${bgColor}`}
       >
         <h3 className="text-xl font-extrabold text-white drop-shadow-lg tracking-wide">
           {title}
--- apps/web/app/times-tables/content/TimesTableCard.tsx
+++ apps/web/app/times-tables/content/TimesTableCard.tsx
@@ -1,36 +1,45 @@
 import Card from "./Card";
 
+const tables = [
+  { title: "1 Times Table", link: "/1-times-table", bgColor: "bg-yellow-700" },
+  { title: "2 Times Table", link: "/2-times-table", bgColor: "bg-blue-700" },
+  { title: "3 Times Table", link: "/3-times-table", bgColor: "bg-green-700" },
+  { title: "4 Times Table", link: "/4-times-table", bgColor: "bg-purple-700" },
+  { title: "5 Times Table", link: "/5-times-table", bgColor: "bg-red-700" },
+  { title: "6 Times Table", link: "/6-times-table", bgColor: "bg-indigo-700" },
+  { title: "7 Times Table", link: "/7-times-table", bgColor: "bg-teal-700" },
+  { title: "8 Times Table", link: "/8-times-table", bgColor: "bg-pink-700" },
+  { title: "9 Times Table", link: "/9-times-table", bgColor: "bg-gray-700" },
+  {
+    title: "10 Times Table",
+    link: "/10-times-table",
+    bgColor: "bg-orange-700",
+  },
+  { title: "11 Times Table", link: "/11-times-table", bgColor: "bg-cyan-700" },
+  {
+    title: "12 Times Table",
+    link: "/12-times-table",
+    bgColor: "bg-violet-700",
+  },
+];
+
 const TimesTableCard = () => {
   return (
-    <>
-      <div>
-        <div className="pt-6 font-bold">
-          Which multiplication table would you like to master?
-        </div>
-        <div className="flex md:flex-row flex-col  gap-6 pt-2 ">
+    <div className="py-6 ">
+      <h2 className="text-xl font-bold">
+        Which multiplication table would you like to master?
+      </h2>
+      <div className="flex flex-wrap md:flex-row flex-col gap-4 pt-2 md:items-start items-center">
+        {tables.map((table, index) => (
           <Card
-            title="1 Times Table"
-            link="/1-times-table"
-            bgColor="bg-yellow-700"
+            key={index}
+            title={table.title}
+            link={table.link}
+            bgColor={table.bgColor}
           />
-          <Card
-            title="2 Times Table"
-            link="/2-times-table"
-            bgColor="bg-blue-500"
-          />
-          <Card
-            title="3 Times Table"
-            link="/3-times-table"
-            bgColor="bg-green-700"
-          />
-          <Card
-            title="4 Times Table"
-            link="/4-times-table"
-            bgColor="bg-purple-700"
-          />
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
