eb4b9d020aa2746aacbe27c08b76297dc186ed2a
Sourav <minulhasan.dev@gmail.com>
2025-03-23 17:49:42 +0600

    Integrate gameengine.

--- apps/web/app/games/2048-game/Game/index.tsx
+++ apps/web/app/games/2048-game/Game/index.tsx
@@ -1,50 +1,20 @@
 "use client";
 import { useEffect } from "react";
 import "./game.css";
+import startGame from "./gameengine";
 
 const Game2048 = () => {
   useEffect(() => {
-    const srcUrls = [
-      "/2048/animframe_polyfill.js",
-      "/2048/application.js",
-      "/2048/bind_polyfill.js",
-      "/2048/classlist_polyfill.js",
-      "/2048/game_manager.js",
-      "/2048/grid.js",
-      "/2048/html_actuator.js",
-      "/2048/keyboard_input_manager.js",
-      "/2048/local_storage_manager.js",
-      "/2048/tile.js",
-    ];
-
-    const scripts: HTMLScriptElement[] = [];
-    srcUrls.forEach((url) => {
-      const script = document.createElement("script");
-      script.src = url;
-      script.async = true;
-      document.body.appendChild(script);
-      scripts.push(script);
-    });
-
-    return () => {
-      scripts.forEach((script) => document.body.removeChild(script));
-    };
+    startGame();
   }, []);
   return (
     <div className="container-root">
-      <div className="heading">
-        <h1 className="title">2048</h1>
-        <div className="scores-container">
+      <div className="flex justify-between">
+        <a className="restart-button mt-2">New Game</a>
+        <div className="scores-container flex gap-2">
           <div className="score-container">0</div>
           <div className="best-container">0</div>
         </div>
-      </div>
-
-      <div className="above-game">
-        <p className="game-intro">
-          Join the numbers and get to the <strong>2048 tile!</strong>
-        </p>
-        <a className="restart-button">New Game</a>
       </div>
 
       <div className="game-container">
@@ -85,12 +55,6 @@ const Game2048 = () => {
 
         <div className="tile-container"></div>
       </div>
-
-      <p className="game-explanation">
-        <strong className="important">How to play:</strong> Use your{" "}
-        <strong>arrow keys</strong> to move the tiles. When two tiles with the
-        same number touch, they <strong>merge into one!</strong>
-      </p>
     </div>
   );
 };
