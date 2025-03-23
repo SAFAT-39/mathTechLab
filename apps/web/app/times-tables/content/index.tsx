eda031a21b2d0e2dec42c78e2b33fcf2e5f71ea2
Safat khan <safat@kitegamesstudio.com>
2025-03-23 21:46:54 +0600

    Integrate interactive multiplication chart  functionality and how to use section.

--- apps/web/app/times-tables/content/index.tsx
+++ apps/web/app/times-tables/content/index.tsx
@@ -1,10 +1,12 @@
 import TimesTableCard from "./TimesTableCard";
 import MultiplicationTable from "./MultiplicationTable";
+import MultiplicationActivityChart from "./MultiplicationActivityChart";
 const Content = () => {
   return (
     <>
       <TimesTableCard />
       <MultiplicationTable />
+      <MultiplicationActivityChart />
     </>
   );
 };
