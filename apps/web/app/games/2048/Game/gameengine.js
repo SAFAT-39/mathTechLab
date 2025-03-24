1ccb20d3056f47e12fc29b09e91e2655284fac8e
Sourav <minulhasan.dev@gmail.com>
2025-03-25 01:22:20 +0600

    fix bug.

--- apps/web/app/games/2048/Game/gameengine.js
+++ apps/web/app/games/2048/Game/gameengine.js
@@ -617,13 +617,13 @@ HTMLActuator.prototype.updateScore = function (score) {
 
   this.scoreContainer.textContent = this.score;
 
-  if (difference > 0) {
-    var addition = document.createElement("div");
-    addition.classList.add("score-addition");
-    addition.textContent = "+" + difference;
+  // if (difference > 0) {
+  //   var addition = document.createElement("div");
+  //   addition.classList.add("score-addition");
+  //   addition.textContent = "+" + difference;
 
-    this.scoreContainer.appendChild(addition);
-  }
+  //   this.scoreContainer.appendChild(addition);
+  // }
 };
 
 HTMLActuator.prototype.updateBestScore = function (bestScore) {
