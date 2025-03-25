251a426167d2bb34899311a66e2031fbe148b770
Safat khan <safat@kitegamesstudio.com>
2025-03-26 02:24:26 +0600

    Integrate Multiplication Practice In Sequence step.

--- apps/web/app/times-tables/1-times-table/page.tsx
+++ apps/web/app/times-tables/1-times-table/page.tsx
@@ -1,13 +1,21 @@
 import TimesTableSection from "../uitils/timesTablsSection1/TimesTableSection1";
+import MulPracticeInSequence from "../uitils/timesTablesSection2/MulPracticeInSequence";
 
 export default function OneTimesTable() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
-      <TimesTableSection
-        tableNumber={1}
-        titleGradient="bg-gradient-to-r from-blue-600 to-violet-600"
-        cardGradient="bg-gradient-to-br from-violet-500 to-purple-600"
+    <>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
+        <TimesTableSection
+          tableNumber={1}
+          titleGradient="bg-gradient-to-r from-blue-600 to-violet-600"
+          cardGradient="bg-gradient-to-br from-violet-500 to-purple-600"
+        />
+      </div>
+      <MulPracticeInSequence
+        timesTable={1}
+        bgGradient="from-blue-600 to-violet-600"
+        buttonGradient="from-violet-600 to-blue-600"
       />
-    </div>
+    </>
   );
 }
