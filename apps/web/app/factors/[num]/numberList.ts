60b223f293e1915ffe1edfbf00b14eb68f62decc
Sourav <minulhasan.dev@gmail.com>
2025-04-10 02:00:08 +0600

    Update numberList.

--- apps/web/app/factors/[num]/numberList.ts
+++ apps/web/app/factors/[num]/numberList.ts
@@ -1,3 +1,8 @@
-const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const firstList = Array.from({ length: 256 }, (_, i) => i + 1);
+const secondList = [
+  258, 260, 270, 275, 280, 287, 288, 289, 300, 320, 343, 360, 361, 378, 397,
+  400, 403, 415, 420, 500, 529, 600, 625, 675, 700, 729, 742, 800, 900, 1000,
+];
+const numberList = [...firstList];
 
 export default numberList;
