2880914c7fd3e109a21b8acb17296b179e3672eb
Sourav <minulhasan.dev@gmail.com>
2025-04-14 16:07:52 +0600

    Update sitemap.

--- apps/web/app/calculators/sitemapUtil.ts
+++ apps/web/app/calculators/sitemapUtil.ts
@@ -2,27 +2,32 @@ const getCalculatorsPages = () => {
   return [
     {
       url: "https://mathtechlab.com/calculators",
-      lastModified: "2025-04-12",
+      lastModified: "2025-04-14",
       priority: 0.8,
     },
     {
       url: "https://mathtechlab.com/calculators/factor-checker",
-      lastModified: "2025-04-12",
+      lastModified: "2025-04-14",
       priority: 0.8,
     },
     {
       url: "https://mathtechlab.com/calculators/prime-factorization-calculator",
-      lastModified: "2025-04-12",
+      lastModified: "2025-04-14",
       priority: 0.8,
     },
     {
       url: "https://mathtechlab.com/calculators/gcf-calculator",
-      lastModified: "2025-04-12",
+      lastModified: "2025-04-14",
       priority: 0.8,
     },
     {
       url: "https://mathtechlab.com/calculators/lcm-calculator",
-      lastModified: "2025-04-12",
+      lastModified: "2025-04-14",
+      priority: 0.8,
+    },
+    {
+      url: "https://mathtechlab.com/calculators/fraction-calculator",
+      lastModified: "2025-04-14",
       priority: 0.8,
     },
   ];
