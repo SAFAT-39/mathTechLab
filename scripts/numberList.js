7fc56a617a7d459a215e191966cdb6a4001f9ca4
Sourav <minulhasan.dev@gmail.com>
2025-04-24 22:41:05 +0600

    Add factors image generation scripts.

---
+++ scripts/numberList.js
@@ -0,0 +1,9 @@
+const firstList = Array.from({ length: 255 }, (_, i) => i + 2);
+const secondList = [
+  258, 260, 270, 275, 280, 287, 288, 289, 300, 320, 343, 360, 361, 378, 397,
+  400, 403, 415, 420, 500, 512, 529, 600, 625, 675, 700, 729, 742, 800, 900,
+  1000, 1024,
+];
+const numberList = [120, 840];// [...firstList, ...secondList];
+
+module.exports = numberList; 
