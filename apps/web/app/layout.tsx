45ea1dde27331090182bb34dcf840e6379072a9c
Sourav <minulhasan.dev@gmail.com>
2025-10-06 02:10:16 +0600

    Add ezoic scripts.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -59,6 +59,14 @@ export default function Layout({ children }: LayoutProps) {
         ></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6350189879354435"
           crossOrigin="anonymous"></script>
+        <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+        <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+
+        <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+        <script>
+          window.ezstandalone = window.ezstandalone || { };
+          ezstandalone.cmd = ezstandalone.cmd || [];
+        </script>
       </head>
 
       <body>
