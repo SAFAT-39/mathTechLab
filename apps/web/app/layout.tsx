53f26cc4698dea1dc70c3cd3e14545235f9cfccb
Sourav <minulhasan.dev@gmail.com>
2025-10-06 02:27:33 +0600

    Fix ezoic script.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -63,10 +63,14 @@ export default function Layout({ children }: LayoutProps) {
         <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
 
         <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
-        <script>
-          window.ezstandalone = window.ezstandalone || { };
-          ezstandalone.cmd = ezstandalone.cmd || [];
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.ezstandalone = window.ezstandalone || {};
+              ezstandalone.cmd = ezstandalone.cmd || [];
+            `,
+          }}
+        />
       </head>
 
       <body>
