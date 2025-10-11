924a5f2fa1c15fff313f22c8b9a5b0c528de80b2
Sourav <minulhasan.dev@gmail.com>
2025-10-11 21:56:29 +0600

    Comment out Gatekeeper consent scripts in layout.tsx for cleaner code and potential future use.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -59,7 +59,7 @@ export default function Layout({ children }: LayoutProps) {
         ></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6350189879354435"
           crossOrigin="anonymous"></script>
-        <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+        {/* <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
         <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
 
         <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
@@ -70,7 +70,7 @@ export default function Layout({ children }: LayoutProps) {
               ezstandalone.cmd = ezstandalone.cmd || [];
             `,
           }}
-        />
+        /> */}
       </head>
 
       <body>
